2e4a5d198b0eb18300c238238047008ffa888ca5
GilbertRT <gramosti@unsa.edu.pe>
2024-09-02 11:46:39 -0500

    Skillls updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { BiLogoReact, BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoGithub } from 'react-icons/bi';
-import { FaNode, FaPython, FaJava } from "react-icons/fa";
+import { BiLogoReact, BiLogoHtml5, BiLogoCss3, BiLogoJavascript } from 'react-icons/bi';
+import { FaNode, FaPython, FaJava, FaLaravel } from "react-icons/fa";
+import { IoLogoVue } from "react-icons/io5";
 import { IconContext } from 'react-icons/lib';
 import { SiMysql, SiVite, SiTailwindcss, SiDjango, SiMongodb, SiSqlite } from 'react-icons/si';
 
@@ -14,34 +15,36 @@ export default function Skills() {
   const iconColor = 'black';
 
   return (
-    <div className='flex flex-col w-full pt-16 px-4 items-center'>
-      <h1 className='w-min text-5xl font-bold text-main-color mb-10'>{t('skillsTitle')}</h1>
+    <div className='flex flex-col items-center w-full px-4 pt-16'>
+      <h1 className='mb-10 text-5xl font-bold w-min text-main-color'>{t('skillsTitle')}</h1>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 m-10 p-10 w-full'>
-        <div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
-          <h2 className='text-2xl font-bold mb-4 text-center'>{t('frontendTechnologies')}</h2>
+      <div className='grid w-full grid-cols-1 gap-4 p-10 m-10 md:grid-cols-2'>
+        <div className='flex flex-col items-center p-6 bg-white rounded-lg shadow-md'>
+          <h2 className='mb-4 text-2xl font-bold text-center'>{t('frontendTechnologies')}</h2>
           <div className='flex flex-wrap justify-center gap-4'>
             <IconContext.Provider value={{ color: iconColor, size: iconSize }}>
               <Tooltip text="HTML5"><BiLogoHtml5 /></Tooltip>
               <Tooltip text="CSS3"><BiLogoCss3 /></Tooltip>
               <Tooltip text="React"><BiLogoReact /></Tooltip>
               <Tooltip text="Vite"><SiVite /></Tooltip>
+              <Tooltip text="Vue.js"><IoLogoVue /></Tooltip>
               <Tooltip text="Tailwind CSS"><SiTailwindcss /></Tooltip>
             </IconContext.Provider>
           </div>
         </div>
-        <div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
-          <h2 className='text-2xl font-bold mb-4 text-center'>{t('backendTechnologies')}</h2>
+        <div className='flex flex-col items-center p-6 bg-white rounded-lg shadow-md'>
+          <h2 className='mb-4 text-2xl font-bold text-center'>{t('backendTechnologies')}</h2>
           <div className='flex flex-wrap justify-center gap-4'>
             <IconContext.Provider value={{ color: iconColor, size: iconSize }}>
               <Tooltip text="JavaScript"><BiLogoJavascript /></Tooltip>
               <Tooltip text="Node.js"><FaNode /></Tooltip>
               <Tooltip text="Django"><SiDjango /></Tooltip>
+              <Tooltip text="Laravel"><FaLaravel /></Tooltip>
             </IconContext.Provider>
           </div>
         </div>
-        <div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
-          <h2 className='text-2xl font-bold mb-4 text-center'>{t('databases')}</h2>
+        <div className='flex flex-col items-center p-6 bg-white rounded-lg shadow-md'>
+          <h2 className='mb-4 text-2xl font-bold text-center'>{t('databases')}</h2>
           <div className='flex flex-wrap justify-center gap-4'>
             <IconContext.Provider value={{ color: iconColor, size: iconSize }}>
               <Tooltip text="MySQL"><SiMysql /></Tooltip>
@@ -50,8 +53,8 @@ export default function Skills() {
             </IconContext.Provider>
           </div>
         </div>
-        <div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
-          <h2 className='text-2xl font-bold mb-4 text-center'>{t('programmingLanguages')}</h2>
+        <div className='flex flex-col items-center p-6 bg-white rounded-lg shadow-md'>
+          <h2 className='mb-4 text-2xl font-bold text-center'>{t('programmingLanguages')}</h2>
           <div className='flex flex-wrap justify-center gap-4'>
             <IconContext.Provider value={{ color: iconColor, size: iconSize }}>
               <Tooltip text="Python"><FaPython /></Tooltip>
